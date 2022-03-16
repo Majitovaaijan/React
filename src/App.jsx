36361e0f3c9eb86d4ad5c5d05bcf12d14121c069
Aijan <aijanka.9239@gmail.com>
2022-03-16 12:44:10 +0600

    feat: added task

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Entry from "./Entry";
+const App = () => {
+  return(
+      <div>
+          <Header/>
+           <Entry/>
+          <Footer/>
+      </div>
+  )
+}
+
+export default App;

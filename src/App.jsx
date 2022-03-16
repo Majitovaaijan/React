d95ba3b056a4f0189ab486f3651a781814b56842
Aijan <aijanka.9239@gmail.com>
2022-03-16 23:07:55 +0600

    feat: added task

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Entry from "./Entry";
+import Title from "./Title";
+import EntryMain from "./EntryMain";
+import AboutMySelf from "./AboutMySelf";
+import Posts from "./Posts";
+import Tags from "./Tags";
+
 const App = () => {
   return(
       <div>
           <Header/>
-           <Entry/>
+          <div className="w3-row">
+              <div className="w3-col l8 s12">
+                  <Title/>
+                  <EntryMain/>
+              </div>
+              <div className="w3-col l4">
+                   <AboutMySelf/>
+                   <Posts/>
+                   <Tags/>
+              </div>
+          </div>
           <Footer/>
       </div>
   )
